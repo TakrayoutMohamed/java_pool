10eba1c83e718ce637919e2dfc33d2a0de4a1730
Mohamed Takrayout <mohtakra@e1r7p11.1337.ma>
2023-06-17 20:07:16 +0100

    ex01 Module00

---
+++ module00/ex01/Programe.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+import java.lang.Boolean;
+
+class   Programe
+{
+    private static boolean isDivider(int dividedBy, int nbr)
+    {
+        return ((nbr % dividedBy != 0) ? false : true);
+    }
+
+    public static void main(String[] args)
+    {
+        int nbr;
+        int i;
+
+        i = 2;
+        System.out.print("-> ");
+        Scanner sc = new Scanner(System.in);
+        nbr = sc.nextInt();
+        sc.close();
+        if (nbr < 2)
+        {
+            System.err.format("IllegalArgument");
+            System.exit(-1);
+        }
+        while (i <= Math.round(Math.sqrt(nbr)))
+        {
+            if (isDivider(i, nbr))
+            {
+                System.out.print("false "+ (i - 1));
+                break ;
+            }
+            if (i == Math.round(Math.sqrt(nbr)))
+            {
+                System.out.println("true "+(i - 1));
+            }
+            i++;
+        }
+    }
+}
